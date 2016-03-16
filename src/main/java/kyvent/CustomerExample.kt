2eec253839c20f04aa6f5b2719c94f60d7397950
rodolfodpk <rodolfo_dpk@yahoo.com>
2016-03-16 02:56:18 -0300

    possible solution

--- src/main/java/kyvent/CustomerExample.kt
+++ src/main/java/kyvent/CustomerExample.kt
@@ -1,8 +1,56 @@
 package kyvent
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.databind.DatabindContext
+import com.fasterxml.jackson.databind.JavaType
+import com.fasterxml.jackson.databind.annotation.JsonTypeIdResolver
+import com.fasterxml.jackson.databind.jsontype.TypeIdResolver
 import java.time.LocalDateTime
 
+// https://www.thomaskeller.biz/blog/2013/09/10/custom-polymorphic-type-handling-with-jackson/
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.CUSTOM,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "customerCommand")
+@JsonTypeIdResolver(CommandTypeIdResolver::class)
+abstract class CustomerCommand: Command
+
+class CommandTypeIdResolver: TypeIdResolver {
+    override fun idFromBaseType(): String? {
+        throw UnsupportedOperationException()
+    }
+
+    override fun init(p0: JavaType?) {
+        throw UnsupportedOperationException()
+    }
+
+    override fun typeFromId(p0: String?): JavaType? {
+        throw UnsupportedOperationException()
+    }
+
+    override fun typeFromId(p0: DatabindContext?, p1: String?): JavaType? {
+        throw UnsupportedOperationException()
+    }
+
+    override fun getDescForKnownTypeIds(): String? {
+        throw UnsupportedOperationException()
+    }
+
+    override fun idFromValueAndType(p0: Any?, p1: Class<*>?): String? {
+        throw UnsupportedOperationException()
+    }
+
+    override fun getMechanism(): JsonTypeInfo.Id? {
+        throw UnsupportedOperationException()
+    }
+
+    override fun idFromValue(p0: Any?): String? {
+        throw UnsupportedOperationException()
+    }
+}
+
 // commands
+
 
 data class CreateCustomerCmd(override val commandId: CommandId,
                              override val eventSourcedId: EventSourcedId) : Command
