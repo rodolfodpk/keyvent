9bd4a774b670ab0afb39b16b5e03f27ca2310911
rodolfodpk <rodolfo_dpk@yahoo.com>
2016-03-18 20:58:04 -0300

    moved example to test scope

--- src/main/java/kyvent/Main.kt
+++ src/main/java/kyvent/Main.kt
@@ -9,36 +9,37 @@ import com.fasterxml.jackson.module.kotlin.KotlinModule
 fun main(args : Array<String>) {
     //   test1()
     //  test2()
-    jacksonTest()
+    // jacksonTest()
+    println("hi !")
 
 }
-
-fun test1() {
-    val cmd : CreateCustomerCmd = CreateCustomerCmd(CommandId(), CustomerId())
-    val snapshot : Snapshot<Customer> = Snapshot(Customer(), Version(0))
-    val uow = handleCustomerCommands(snapshot, cmd, applyEventOnCustomer)
-}
-
-fun uow2(): CustomerUnitOfWork? {
-    val cmd : CreateActivatedCustomerCmd = CreateActivatedCustomerCmd(CommandId(), CustomerId())
-    val snapshot : Snapshot<Customer> = Snapshot(Customer(), Version(1))
-    return handleCustomerCommands(snapshot, cmd, applyEventOnCustomer)
-}
-
-fun jacksonTest() {
-
-    val mapper = ObjectMapper().registerModules(Jdk8Module(), KotlinModule(), JavaTimeModule())
-    mapper.enable(SerializationFeature.INDENT_OUTPUT);
-    mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-
-    val uow = uow2()
-    val uowAsJson = mapper.writeValueAsString(uow)
-
-    val jsonHelper = JsonHelper<CustomerUnitOfWork>(mapper)
-    val uowFromJson = jsonHelper.fromJson(uowAsJson, CustomerUnitOfWork::class.java)
-
-    val jsonHelper2 = JsonHelper<List<CustomerEvent>>(mapper)
-    val onlyTheEvents: List<CustomerEvent> = jsonHelper2.fromJsonAt("/events", uowAsJson, List::class.java)
-
-
-}
+//
+//fun test1() {
+//    val cmd : CreateCustomerCmd = CreateCustomerCmd(CommandId(), CustomerId())
+//    val snapshot : Snapshot<Customer> = Snapshot(Customer(), Version(0))
+//    val uow = handleCustomerCommands(snapshot, cmd, applyEventOnCustomer)
+//}
+//
+//fun uow2(): CustomerUnitOfWork? {
+//    val cmd : CreateActivatedCustomerCmd = CreateActivatedCustomerCmd(CommandId(), CustomerId())
+//    val snapshot : Snapshot<Customer> = Snapshot(Customer(), Version(1))
+//    return handleCustomerCommands(snapshot, cmd, applyEventOnCustomer)
+//}
+//
+//fun jacksonTest() {
+//
+//    val mapper = ObjectMapper().registerModules(Jdk8Module(), KotlinModule(), JavaTimeModule())
+//    mapper.enable(SerializationFeature.INDENT_OUTPUT);
+//    mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+//
+//    val uow = uow2()
+//    val uowAsJson = mapper.writeValueAsString(uow)
+//
+//    val jsonHelper = JsonHelper<CustomerUnitOfWork>(mapper)
+//    val uowFromJson = jsonHelper.fromJson(uowAsJson, CustomerUnitOfWork::class.java)
+//
+//    val jsonHelper2 = JsonHelper<List<CustomerEvent>>(mapper)
+//    val onlyTheEvents = jsonHelper2.fromJsonAt("/events", uowAsJson, List::class.java)
+//
+//
+//}
--- src/test/java/kyvent/MapJournalSpec.kt
+++ src/test/java/kyvent/MapJournalSpec.kt
@@ -3,7 +3,6 @@ package kyvent
 import org.jetbrains.spek.api.Spek
 import java.time.LocalDateTime
 import kotlin.test.assertEquals
-import kotlin.test.assertFailsWith
 import kotlin.test.assertTrue
 
 class MapJournalSpec: Spek() {
@@ -13,7 +12,7 @@ class MapJournalSpec: Spek() {
             on("adding a new unitOfWork") {
                 val cmd: CreateCustomerCmd = CreateCustomerCmd(CommandId(), CustomerId())
                 val uow = CustomerUnitOfWork(customerCommand = cmd, version = Version(1), events = listOf(CustomerCreated(cmd.customerId)))
-                journal.append(cmd.customerId, uow!!)
+                journal.append(cmd.customerId, uow)
                 it("should result in a journal with the respective entry") {
                     val expected: MutableList<CustomerUnitOfWork> = mutableListOf(uow)
                     val current: MutableList<CustomerUnitOfWork>? = journal.map[cmd.customerId]
@@ -28,7 +27,7 @@ class MapJournalSpec: Spek() {
                 val uow = CustomerUnitOfWork(customerCommand = cmd, version = Version(2), events = listOf(CustomerCreated(cmd.customerId)))
                 it("should throw an exception since version should be 1") {
                     try {
-                        journal.append(cmd.customerId, uow!!)
+                        journal.append(cmd.customerId, uow)
                     } catch (e: IllegalArgumentException) {
                         assertTrue(true, "should throw IllegalArgumentException")
                     }
@@ -40,12 +39,12 @@ class MapJournalSpec: Spek() {
             val uow1 = CustomerUnitOfWork(customerCommand= createCustomerCmd,
                                           version = Version(1),
                                           events = listOf(CustomerCreated(createCustomerCmd.customerId)))
-            val journal = MapJournal(map = mutableMapOf(Pair(createCustomerCmd.customerId, mutableListOf(uow1!!))),
+            val journal = MapJournal(map = mutableMapOf(Pair(createCustomerCmd.customerId, mutableListOf(uow1))),
                     versionExtractor = { uow -> uow.version })
             on("adding a new unitOfWork") {
                 val activateCmd: ActivateCustomerCmd = ActivateCustomerCmd(CommandId(), createCustomerCmd.customerId)
                 val uow2 = CustomerUnitOfWork(customerCommand = activateCmd, version = Version(2), events = listOf(CustomerActivated(LocalDateTime.now())))
-                journal.append(createCustomerCmd.customerId, uow2!!)
+                journal.append(createCustomerCmd.customerId, uow2)
                 it("should result in a journal with the respective first and second entries") {
                     val expected: MutableList<CustomerUnitOfWork> = mutableListOf(uow1, uow2)
                     val current: MutableList<CustomerUnitOfWork>? = journal.map[createCustomerCmd.customerId]
