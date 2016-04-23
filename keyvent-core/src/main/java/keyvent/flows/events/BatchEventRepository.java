35bfe939f47e88bb0dab4f0c92d4602b81ddac23
rodolfodpk <rodolfo_dpk@yahoo.com>
2016-04-22 22:16:56 -0300

    fixed test folders

--- keyvent-core/src/main/java/keyvent/flows/events/BatchEventRepository.java
+++ keyvent-core/src/main/java/keyvent/flows/events/BatchEventRepository.java
@@ -3,7 +3,7 @@ package keyvent.flows.events;
 import javaslang.collection.List;
 import keyvent.data.GlobalEventSeq;
 
-public interface BoundedContextEventRepository<ID, UOW> {
+public interface BatchEventRepository<ID, UOW> {
 
     List<UOW> eventsAfter(GlobalEventSeq globalEventSequence, int limit);
 
